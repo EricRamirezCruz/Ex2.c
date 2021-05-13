a6f20441e5713b14efc5d56ff703ecbba2e15650
EricRamirezCruz <82549890+EricRamirezCruz@users.noreply.github.com>
2021-05-12 21:50:38 -0500

    Add files via upload

---
+++ Ex2.c
@@ -0,0 +1,19 @@
+  
+#include <stdio.h>
+int main() 
+{
+    /* Eric Ramirez Cruz
+    12 MAY 21
+    Programa 2. Pide dos valores enteros, los suma y muestra resultado
+    */
+   int a, b, c;
+   printf ("\n \n \n \nPrograma 2 \n");
+   printf ("Suma de numeros enteros \n");
+   printf ("\n \nDame el valor entero de a: ");
+   scanf ("%d",&a);            
+   printf ("\n \nDame el valor entero de b: ");
+   scanf ("%d",&b); 
+   c = a + b;
+   printf ("\n El resultado es %d",c);
+   return 0;
+}
